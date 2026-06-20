81a3bc2eb08954e18f3b851b538d661efa9431ea
backseatmediasheets <daviddmadjarov@gmail.com>
2026-06-20 11:15:39 +0200

    google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { SiteFooter } from './site-footer';
 import { ThemeProvider } from './theme-provider';
@@ -24,6 +25,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7038021317385938"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className="bg-dots min-h-screen">
         <ThemeProvider>
           <SoundToggleProvider>
